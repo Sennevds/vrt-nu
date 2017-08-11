820cdeb1d944b3e1f4db9a30a53c0be21969ba5c
Steven Volckaert <steven.volckaert@gmail.com>
2017-08-11 14:05:29 +0200

    Adding MainTvActivity.

---
+++ src/StevenVolckaert.VrtNu.Android/MainTvActivity.cs
@@ -0,0 +1,24 @@
+﻿namespace StevenVolckaert.VrtNu.Android
+{
+    using global::Android.App;
+    using global::Android.Content;
+    using global::Android.Content.PM;
+    using global::Android.OS;
+
+    [Activity(
+        Label = "@string/ApplicationName"
+        , Icon = "@drawable/vrt_nu_banner"
+        , Theme = "@style/Theme.Leanback"
+        , ScreenOrientation = ScreenOrientation.Landscape
+        //, MainLauncher = true
+    )]
+    [IntentFilter(new[] { Intent.ActionMain }, Categories = new string[] { Intent.CategoryLeanbackLauncher })]
+    public class MainTvActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.MainTvLayout);
+        }
+    }
+}
