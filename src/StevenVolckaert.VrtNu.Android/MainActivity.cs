954adae070ef0549503c7bd75673d95a8e886ff5
Steven Volckaert <steven.volckaert@gmail.com>
2017-08-11 14:13:34 +0200

    Disabling MainActivity, for now. It is replaced by MainTvActivity but is likely going to be used later.

--- src/StevenVolckaert.VrtNu.Android/MainActivity.cs
+++ src/StevenVolckaert.VrtNu.Android/MainActivity.cs
@@ -2,20 +2,19 @@
 {
     using global::Android.App;
     using global::Android.OS;
-    using global::Android.Widget;
 
     [Activity(
         Label = "@string/ApplicationName",
         Icon = "@drawable/vrt_nu_banner",
-        ScreenOrientation = global::Android.Content.PM.ScreenOrientation.Landscape,
-        MainLauncher = true
+        MainLauncher = true,
+        Enabled = false
     )]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-            SetContentView(Resource.Layout.Main);
+            //SetContentView(Resource.Layout.Main);
         }
     }
 }
