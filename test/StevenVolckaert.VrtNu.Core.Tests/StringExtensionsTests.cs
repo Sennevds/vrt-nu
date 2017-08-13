15a569ebbb9346e6a9e4a2c3d3407d7756e171a5
Steven Volckaert <steven.volckaert@gmail.com>
2017-08-13 17:30:06 +0200

    Adding unit test project for StevenVolckaert.VrtNu.Core.

---
+++ test/StevenVolckaert.VrtNu.Core.Tests/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿namespace StevenVolckaert.VrtNu.Core.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Xunit;
+
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void AsJsonStringTest()
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
